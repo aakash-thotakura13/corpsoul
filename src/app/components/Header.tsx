595d9d6fc862fb19c99c5146d7a5397a4285f01e
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-09-24 16:13:48 +0530

    added zindex to put the Header component on the top of remaining components

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -24,7 +24,7 @@ export default function HeaderComponent() {
   const closeMobileMenu = () => setIsMobileMenuOpen(false);
 
   return (
-    <header style={{ maxWidth: "1200px", minWidth: "350px", margin: "0em auto", position: "sticky", top: 0, backgroundColor: "white", padding: "0em 1em", }}>
+    <header style={{ maxWidth: "1200px", minWidth: "350px", margin: "0em auto", position: "sticky", top: 0, backgroundColor: "white", padding: "0em 1em", zIndex: 999 }}>
 
       {/* Main header */}
       <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", position: "sticky", top: 0, padding: "0.5em 0", }}>
